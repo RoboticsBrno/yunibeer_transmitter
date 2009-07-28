340481de1b017d466930c4c2829305819db303d5
Martin Vejnar <avakar@ratatanek.cz>
2009-07-28 16:21:37 +0200

    .

--- yunibeer_transmitter.c
+++ yunibeer_transmitter.c
@@ -4,50 +4,44 @@
 #include "../avrlib/format.hpp"
 using namespace avrlib;
 
-int8_t get_pot(int index)
+int16_t get_pot(int index)
 {
-	ADMUX = (1<<ADLAR)|(index + 4);
+	static uint8_t const channels[] = { 4, 5, 6, 7 };
+	static bool const invert[] = { true, false, false, false };
+
+	ADMUX = (1<<ADLAR) | channels[index];
 	ADCSRA |= (1<<ADSC);
-	while (ADCSRA & (1<<ADSC))
+	while ((ADCSRA & (1<<ADIF)) == 0)
 	{
 	}
 	ADCSRA |= (1<<ADSC);
-	while (ADCSRA & (1<<ADSC))
+	while ((ADCSRA & (1<<ADIF)) == 0)
 	{
 	}
 
-	int8_t res = ADCH - 128;
+	uint16_t res = ADCL;
+	res |= ADCH << 8;
 
-	if (res > 20)
-		res -= 20;
-	else if (res < -20)
-		res += 20;
-	else
-		res = 0;
+	if (invert[index])
+		res = -res;
 
-	return -res;
+	return int16_t(res - 0x8000);
 }
 
 uint8_t get_buttons()
 {
-	return PINB & 0xfe;
+	return PINB;
 }
 
 int main()
 {
 	sync_usart<usart1, bootseq> rs232(38400);
 
-	for (int j = 0; j < 10000; ++j)
-	{
-		asm __volatile__ ("nop");
-	}
-
-	send(rs232, "'1' -- text, '2' -- binary");
-
 	DDRB = 0;
 	PORTB = 0xff;
+	PORTF = 0;
 	
-	ADMUX = (1<<ADLAR)|7;
+//	ADMUX = (1<<ADLAR)|7;
 	ADCSRA = (1<<ADEN)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0);
 
 	TCCR0 = (1<<CS02)|(1<<CS01)|(1<<CS00);
@@ -70,6 +64,10 @@ int main()
 				send_state = 2;
 				PORTC ^= (1<<5)|(1<<7);
 				break;
+			case '?':
+				send_state = 0;
+				send(rs232, "'1' -- text, '2' -- binary\r\n");
+				break;
 			default:
 				send_state = 0;
 			}
@@ -83,17 +81,16 @@ int main()
 			{
 			case 1:
 				for (int i = 0; i < 4; ++i)
-					send_int(rs232, get_pot(i), 5);
+					send_int(rs232, get_pot(i), 7);
 				send_int(rs232, get_buttons(), 5);
 				send(rs232, "\r\n");
 				break;
 			case 2:
-				rs232.write(-128);
-				rs232.write(get_pot(0));
-				rs232.write(-get_pot(2));
-				rs232.write(get_pot(1));
-				rs232.write(get_pot(3));
-				rs232.write(get_buttons());
+				rs232.write(0x80);
+				rs232.write(0x19);
+				for (int i = 0; i < 4; ++i)
+					send_bin(rs232, get_pot(i));
+				send_bin(rs232, get_buttons());
 				break;
 			}
 		}
